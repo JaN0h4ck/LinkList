5240c244e65bd9dca796d2415a11bad370732eb8
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2023-11-12 14:29:06 +0100

    Added footer and Mastodon verification

---
+++ index.e91d2de5.js
@@ -0,0 +1,3 @@
+const e=document.querySelector("#copyright"),t=document.querySelector("#copyright-name");if(null!=e&&(e.textContent=""),null!=t){let e=new Date;t.innerHTML=`${e.getFullYear()} Jan Nothacker - <a href="https://github.com/JaN0h4ck/LinkList">Source Code</a>`}//# sourceMappingURL=index.e91d2de5.js.map
+
+//# sourceMappingURL=index.e91d2de5.js.map
