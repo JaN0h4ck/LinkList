5e7609c8a6353f55ec845bd7240c43f4a55c19da
Jan Nothacker <git@jan-nothacker.de>
2025-02-01 10:13:26 +0100

    remove the uneccesary modal

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,31 +1,5 @@
 const copyrightSymbol = document.querySelector("#copyright");
 const copyrightText = document.querySelector("#copyright-name");
-const stagedLinkElement = <HTMLParagraphElement>document.getElementById("staged-link-text");
-const linkElements = <Array<HTMLButtonElement>>[...document.querySelectorAll(".stage-link")];
-const modalElement = <HTMLDivElement>document.querySelector(".modal-background");
-const continueLinkElement = <HTMLAnchorElement>document.getElementById("continue-link");
-const modalCancelButton = <HTMLButtonElement>document.querySelector(".btn.btn-pill.btn-dialogue.btn-cancel");
-const modalContent = <HTMLDivElement>document.querySelector(".modal-container");
-
-linkElements.forEach((button) => {
-	button.addEventListener("click", () => {
-		modalElement.classList.remove("hidden");
-		stagedLinkElement.textContent = button.dataset.linkText!;
-		continueLinkElement.href = button.dataset.link!;
-	});
-});
-
-modalCancelButton.addEventListener("click", () => {
-	modalElement.classList.add("hidden");
-});
-
-modalElement.addEventListener("click", (e) => {
-	modalElement.classList.add("hidden");
-});
-
-modalContent.addEventListener("click", (e) => {
-	e.stopPropagation();
-});
 
 if (copyrightSymbol != null) {
 	copyrightSymbol.textContent = "\uF7DB";
